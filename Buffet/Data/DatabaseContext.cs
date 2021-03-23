17c0e6bfff587299ae79606322fc12ef9386c143
Gabriel Dutra <gabrielcbdutra@gmail.com>
2021-03-22 22:11:36 -0300

    Aula 22/03 - Buffet

---
+++ Buffet/Data/DatabaseContext.cs
@@ -0,0 +1,17 @@
+using Buffet.Models.Buffet.Cliente;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buffet.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        
+        public DbSet<ClienteEntity> Clientes { get; set; }
+            
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        :base(options)
+        {
+            
+        }
+    }
+}
--- Buffet/Models/Buffet/Cliente/ClienteEntity.cs
+++ Buffet/Models/Buffet/Cliente/ClienteEntity.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Buffet.Models.Buffet.Cliente
 {
     public class ClienteEntity
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
+        public int Id { get; set; } 
+        [Required] public string Nome { get; set; }
         public DateTime DataDeNascimento { get; set; }
         public int Idade { get; set; }
     }
